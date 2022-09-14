3087f2f3277336d71023a36e65f697a430cb6e8b
Andrew Malozemoff <ffomezolam@gmail.com>
2022-09-14 09:42:39 -0400

    utils.py now has working function to split weather conditions

--- code/utils.py
+++ code/utils.py
@@ -2,84 +2,67 @@ import pandas as pd
 import numpy as np
 import re
 
-# words to search for per category
-words = {
+# regex for splitting weather condition entries
+re_wc_split = re.compile('\sand\s|\swith\s|\s\/\s')
+
+# wc_words to search for per category
+# these are chosen arbitrarily based on examination of unique values
+wc_words = {
     'rain': ['rain', 'drizzle', 'precipitation', 'mist'],
     'snow': ['snow', 'wint'],
     'ice': ['hail', 'freez', 'sleet'],
     'wind': ['wind', 'squall'],
     'clouds': ['cloud', 'overcast'],
-    'thunder': ['thunderstorm', 't-storm', 'thunder'],
+    'thunder': ['t-storm', 'thunder'],
     'fog': ['fog', 'haze'],
     'fair': ['fair', 'clear'],
     'tornado': ['tornado', 'funnel'],
-    'dust': ['sand', 'dust', 'smoke']
+    'dust': ['sand', 'dust', 'smoke'],
+    'other': []
 }
 
-# match 'and', '/', and 'with' as separators
-re_wc_split = re.compile('\sand\s|\s/\s|\swith\s')
+# column order
+wc_col_names = sorted(wc_words.keys())
 
-def split_weather_condition(df, colname='Weather_Condition'):
-    global words
+# function to split weather condition series into rows per condition
+# pass df['weather_condition'] list to split
+def split_weather_condition(wc_series, empty=np.nan):
+    # supporting function to get a column for a single wc category
+    def get_wc_entries(colname, wc_list):
+        global wc_words
+        nonlocal empty
+        keywords = wc_words[colname]
+        entry = ''
+        for wc in wc_list:
+            for keyword in keywords:
+                if keyword in wc:
+                    if entry: entry += ' / '
+                    entry += wc
+
+        if not entry: return empty
+
+        return entry
 
     # make all items lowercase
-    df[colname] = [t.lower() if type(t) == str else t for t in df[colname]]
+    wc_series = [t.lower() if type(t) == str else t for t in wc_series]
 
     # NOTE n/a precipitation is a category
 
     rows = list()
-    for wc in df[colname]:
-        # for each weather condition row in df...
-        # split on '/' or 'and' if exists
+    for wc in wc_series:
+        # for each weather condition row...
+        # split on delimiting words ('/','and','with')
         if type(wc) != str:
             # is nan
-            rows.append(np.nan)
+            rows.append([''])
         else:
             rows.append(re.split(re_wc_split, wc))
 
-    # newcols will keep all the data for the new dataframe
-    newcols = dict()
+    wc_dict = {k: list() for k in wc_col_names}
 
-    # didcol will keep data on whether we've touched a category already
-    didcol = dict()
-
-    # initialize both dicts
-    for cat in words:
-        newcols[cat] = list()
-        didcol[cat] = False
-
-    # quick function to reset didcol
-    def reset_didcol():
-        nonlocal didcol
-        for k in didcol:
-            didcol[k] = False
-
-    # go through each row and separate into weather condition entries
+    # get each row and add to wc_dict
     for row in rows:
-        # for each row
-        reset_didcol()
-        for wc in row:
-            # for each weather condition in the row
-            for cat in words:
-                if didcol[cat]:
-                    # if we have multiple entries in a single weather condition
-                    # then we should ignore all after the first - have to discuss
-                    # after examination
-                    print(f'did {cat}: {wc}')
-                    continue
-                # for each category of weather condition
-                for word in cat:
-                    # test against words of that category
-                    if word in wc:
-                        # have a word in weather condition
-                        # so add weather_condition to newcols dict
-                        newcols[cat].append(wc)
-                        didcol[cat] = True
-                        break
-                    else:
-                        # word does not exist in weather condition
-                        newcols[cat].append('0')
-                        break
+        for col in wc_dict:
+            wc_dict[col].append(get_wc_entries(col, row))
 
-
-    return newcols
+    return wc_dict
